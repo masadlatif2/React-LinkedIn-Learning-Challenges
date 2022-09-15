a1f57c4feed4d034903f34b03df17fb6ce16702a
CINNOVA\alatif <alatif@cinnova.com>
2022-09-15 16:19:22 +0500

    dark-mode task completed

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,13 @@
+import { useState } from "react"
+
 export default function DarkMode () {
+
+ const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`page ${darkMode && "dark-mode"}`}>
+      <button onClick={()=>setDarkMode(true)} className='dark-mode-button'>Dark Mode</button>
+      <button onClick={()=>setDarkMode(false)} className='light-mode-button'>Light Mode</button>
     </div>
   )
 }
